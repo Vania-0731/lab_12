3907ed3cdffc0f591e51555ddcd9fe0f76de1ac6
Vania <sonalysifuentes@gmail.com>
2025-06-07 18:59:49 -0500

    views created

--- backend/series/views.py
+++ backend/series/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Serie, Category
+from .serializer import SerieSerializer, CategorySerializer
 
-# Create your views here.
+
+class SerieViewSet(viewsets.ModelViewSet):
+	queryset = Serie.objects.all().order_by('-release_date')
+	serializer_class = SerieSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+	queryset = Category.objects.all().order_by('description')
+	serializer_class = CategorySerializer
